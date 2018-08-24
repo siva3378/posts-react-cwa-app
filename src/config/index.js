77dc7976512d73fdecbe2aebbfdfc14dafb47a88
Siva Kumar <siva3378@gmail.com>
2018-08-24 01:27:56 +0100

    to change the config

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 // const LOCAL_URL = 'http://localhost:3000/api'
 const BASE_URL = 'https://posts-loopback-rest-api.herokuapp.com/api';
-const BASE_URL = LOCAL_URL;
+// const BASE_URL = LOCAL_URL;
 
 export const POSTS = {
     endpoint: `${BASE_URL}/Posts`,
